41dbf8bc16c72cc8ccffdfd8e38dd84c4d505ebf
lauretabinakaj <lauretabinakaj@gmail.com>
2022-02-25 09:18:21 +0100

    progetto

---
+++ s5/g1/myscript.js
@@ -0,0 +1,11 @@
+/*Scopo di questo quinto  esercizio è il corretto utilizzo dell’oggetto Date, dei metodi e delle
+proprietà dell’oggetto
+
+Dovrai creare un file js e intregrarlo in un file di progetto (fai attenzione alla giusta
+creazione delle cartelle)
+
+Richiamare la data attuale
+Mostrarla in una pagina HTML
+Mostrare elementi singoli della data
+Creare un sistema per la formattazione in formato europeo
+Mostrare la data formattata in una pagina HTML*/
